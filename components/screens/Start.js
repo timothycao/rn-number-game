79aa6dc62d28fb8b36e731c1ecc0ec36c9ddba9d
Timothy Cao <tcao.pro@gmail.com>
2020-01-31 16:27:02 -0500

    input validation and keyboard functionality

--- components/screens/Start.js
+++ components/screens/Start.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Button, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 import Card from '../Card';
 import Input from '../Input';
 import Colors from '../../constants/colors';
 
 const Start = props => {
+
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const numberInputHandler = inputText => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>New Game</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Your Number</Text>
-        <Input style={styles.input} />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.secondary} /></View>
-          <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary} /></View>
-        </View>
-      </Card>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>New Game</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Your Number</Text>
+          <Input style={styles.input} keyboardType="numeric" maxLength={2} onChangeText={numberInputHandler} value={enteredValue} />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.secondary} /></View>
+            <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary} /></View>
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -43,7 +52,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15
   },
   input: {
-    width: 50
+    width: 50,
+    textAlign: 'center'
   },
   button: {
     width: 100
