61e8c1787cd10de23e49594d66eca16b1c039eff
Timothy Cao <tcao.pro@gmail.com>
2020-02-07 14:41:04 -0500

    font styling

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import React, {useState} from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import Header from './components/Header';
 import Start from './components/screens/Start';
 import Game from './components/screens/Game';
 import GameOver from './components/screens/GameOver';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessCount, setGuessCount] = useState(0);
+  const [loadedData, setLoadedData] = useState(false);
+
+  if (!loadedData) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setLoadedData(true)} onError={(err) => console.log(err)} />;
+  }
 
   const newGameHandler = () => {
     setUserNumber(null);
